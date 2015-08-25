103600f699721fde98d59cc6d5b8c3a009414d02
Jason <jkrasnits@gmail.com>
2015-08-25 11:59:23 -0400

    Added unique name checking to table edit and changed window names

--- CSTMainWindow.m
+++ CSTMainWindow.m
@@ -185,7 +185,7 @@ scrsz = get(0,'ScreenSize');
 mainW = min(mainPnlW, scrsz(3) - 10);
 mainH = min(mainPnlH, scrsz(4) - 100);
 mainPanelPosition = [2, mainH-mainPnlH-2, mainPnlW, mainPnlH];
-mainFigure = figure('Visible','off','Position',[5,40,mainW,mainH], 'Name',['CeleST: Main Window | ' filenames.data],'numbertitle','off', 'menubar', 'none', 'resizefcn', @resizeMainFigure);
+mainFigure = figure('Visible','off','Position',[5,40,mainW,mainH], 'Name',['CeleST: Main Window | Data Location: ' filenames.data],'numbertitle','off', 'menubar', 'none', 'resizefcn', @resizeMainFigure);
 mainPanel = uipanel('parent', mainFigure,'BorderType', 'none','units','pixels', 'position', mainPanelPosition);
 sliderHoriz = uicontrol('parent',mainFigure,'style','slider','position',[0 0 mainW-20 20],'max', 1,'min',0, 'value',0,'callback',@setMainPanelPositionBySliders);
 sliderVert = uicontrol('parent',mainFigure,'style','slider','position',[mainW-20 20 20 mainH-20],'max', max(1,-mainPanelPosition(2)),'min',0, 'value',max(1,-mainPanelPosition(2)),'callback',@setMainPanelPositionBySliders);
@@ -362,6 +362,8 @@ if fileToLog > 1; fclose(fileToLog); end
         try
             if ~isempty(eventdata.NewData) && (~isnumeric(eventdata.NewData) || ~isnan(eventdata.NewData))
                 fileDB(listVideosIdx(eventdata.Indices(1))).(fieldsIni{eventdata.Indices(2)}) = eventdata.NewData;
+                ensureUniqueNames
+                % code to change filenames for segmentation and measures 
                 populateFilters
             else
                 warndlg('Field is not editable or data is faulty','Data Change Error')
